07d45ec66a0f9be74c2434a12043faddf097596e
yinfxs <yinfxs@gmail.com>
2016-06-14 23:07:01 +0800

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+/**
+ * 客户端入口文件
+ * Created by yinfxs on 16-6-14.
+ */
+
+'use strict';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const index_less = require('./public/styles/index.less');
+const MyButtonController = require('./components/MyButtonController');
+
+console.log();
+
+// const App = React.createClass({
+//     render() {
+//         return (<MyButtonController/>);
+//     }
+// });
+
+const app = document.createElement('div');
+document.body.appendChild(app);
+
+ReactDOM.render(<MyButtonController/>, app);
